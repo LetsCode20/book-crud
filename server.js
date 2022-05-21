bde4d65cb2196c0ae45be14d46f557c7b9976a04
Nejmi Yassine <devtwins0@gmail.com>
2022-05-21 18:59:16 +0000

    Fix errors on Deploy App in Heroku

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config();
-
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./bookSchemas');
 const cors = require('cors');
+dotenv.config();
 
 const app = express();
 
 const { connectDB } = require('./config/index');
 connectDB();
+
+const schema = require('./bookSchemas');
 
 app.use('*', cors());
 app.use(
@@ -24,9 +24,9 @@ app.use(
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 4000;
 
